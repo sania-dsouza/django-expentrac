a7925ef50bcb3fc4d92f3786f00ce2bfb2acf68e
Sania <sania.d@myplanet.com>
2020-07-01 13:45:30 -0400

    changes to page style sheet

--- expentrac/tracker/urls.py
+++ expentrac/tracker/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('', views.base_page, name='base'),
+    # path('', views.base_page, name='base'),
     path('', views.login_page, name='login'),
     path('signup', views.signup_page, name='signup'),
 ]
