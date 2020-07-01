f41332cf53caad95c301835cf3b6ac84a5ecb6ed
Sania <sania.d@myplanet.com>
2020-06-30 22:26:07 -0400

    Added base.html

--- expentrac/tracker/urls.py
+++ expentrac/tracker/urls.py
@@ -3,5 +3,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base, name='base')
+    path('', views.base_page, name='base')
 ]
--- expentrac/tracker/views.py
+++ expentrac/tracker/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def base_page(request):
+    return render(request, 'tracker/base.html')
+
