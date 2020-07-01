16760b4a5c9a5db401ce5e079fbe3c3b804dbbdc
Sania <sania.d@myplanet.com>
2020-06-30 23:53:13 -0400

    Add the login skeleton

--- expentrac/tracker/views.py
+++ expentrac/tracker/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 def base_page(request):
     return render(request, 'tracker/base.html')
 
+
+def login_page(request):
+    return render(request, 'tracker/login.html')
