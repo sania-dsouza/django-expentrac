622874681350d82e2d7c72534af7d7eac186fad2
Sania <sania.d@myplanet.com>
2020-07-01 00:58:33 -0400

    Base and login screens (views only)

--- expentrac/tracker/urls.py
+++ expentrac/tracker/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.base_page, name='base'),
-    path('login', views.login_page, name='login'),
+    #path('', views.base_page, name='base'),
+    path('', views.login_page, name='login'),
 ]
