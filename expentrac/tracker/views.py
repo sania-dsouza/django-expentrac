44ab067fab7d11e064ee25657a01ec6300a97ae6
Sania <sania.d@myplanet.com>
2020-07-02 01:43:56 -0400

    Add table customizations, filters for month and categories (view only)

--- expentrac/tracker/views.py
+++ expentrac/tracker/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from .models import Tracker
 
 def base_page(request):
     return render(request, 'tracker/base.html')
@@ -14,4 +14,5 @@ def signup_page(request):
 
 
 def tracker_page(request):
-    return render(request, 'tracker/trackerTable.html')
+    categories = [verb_cat for code, verb_cat in Tracker.CATEGORY_CHOICES];
+    return render(request, 'tracker/trackerTable.html', {'categories': categories})
