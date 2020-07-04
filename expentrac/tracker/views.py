10a88d191ad31172cc6112de849cf66b3ca39489
Sania <sania.d@myplanet.com>
2020-07-04 18:05:38 -0400

    Added Django forms for Login

--- expentrac/tracker/views.py
+++ expentrac/tracker/views.py
@@ -1,12 +1,23 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Tracker
+from .forms import LoginForm
+
 
 def base_page(request):
     return render(request, 'tracker/base.html')
 
 
 def login_page(request):
-    return render(request, 'tracker/login.html')
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            return HttpResponseRedirect('/')
+
+    else:
+        form = LoginForm()
+
+    return render(request, 'tracker/login.html', {'form': form})
 
 
 def signup_page(request):
@@ -16,3 +27,7 @@ def signup_page(request):
 def tracker_page(request):
     categories = [verb_cat for code, verb_cat in Tracker.CATEGORY_CHOICES];
     return render(request, 'tracker/trackerTable.html', {'categories': categories})
+
+
+
+
