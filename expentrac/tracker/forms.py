8d49c7b987ad8c69507a9e254bd0853498669d6b
Sania <sania.d@myplanet.com>
2020-07-05 13:41:19 -0400

    Change the password length for a min and max value

--- expentrac/tracker/forms.py
+++ expentrac/tracker/forms.py
@@ -11,9 +11,18 @@ class SignUpForm(forms.Form):
     last_name = forms.CharField(label="Last name", max_length=30, help_text='30 characters max.', required=False, widget=forms.TextInput(attrs={'placeholder': 'khan'}))
     email = forms.EmailField(label="Email", max_length=60, widget=forms.EmailInput(attrs={'placeholder': 'sara.email@email.com'}))
     username = forms.CharField(label="Username", max_length=15, help_text='15 characters max.', widget=forms.TextInput(attrs={'placeholder': 'sarakhan'}))
-    password = forms.CharField(label="Password", max_length=8, help_text='8 characters min, 12 characters max. There must be two numbers and two special characters each.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
-    cPassword = forms.CharField(label="Confirm Password", help_text='This must match the previous password field.', max_length=8, widget=forms.PasswordInput(attrs={'placeholder': '********'}))
+    password = forms.CharField(label="Password", min_length=8, max_length=12, help_text='8 characters min, 12 characters max. There must be two numbers and two special characters each.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
+    cPassword = forms.CharField(label="Confirm Password", min_length=8, max_length=12, help_text='This must match the previous password field.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
 
+    def clean(self):
+        cleaned_data = super(SignUpForm, self).clean()
+        password = self.cleaned_data.get('password')
+        cpassword = self.cleaned_data.get('cPassword')
+
+        if password and cpassword and password != cpassword:
+            raise forms.ValidationError("Passwords do not match")
+
+        return self.cleaned_data
 
 
 
