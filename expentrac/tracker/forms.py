b25a057e4a68f226ab770995b0a7db1b9621a90f
Sania <sania.d@myplanet.com>
2020-07-05 00:47:44 -0400

    made Last name non-required field

--- expentrac/tracker/forms.py
+++ expentrac/tracker/forms.py
@@ -8,7 +8,7 @@ class LoginForm(forms.Form):
 
 class SignUpForm(forms.Form):
     first_name = forms.CharField(label="First name", max_length=20, widget=forms.TextInput(attrs={'placeholder': 'sara'}))
-    last_name = forms.CharField(label="Last name", max_length=30, widget=forms.TextInput(attrs={'placeholder': 'khan'}))
+    last_name = forms.CharField(label="Last name", max_length=30, required=False, widget=forms.TextInput(attrs={'placeholder': 'khan'}))
     email = forms.EmailField(label="Email", max_length=60, widget=forms.EmailInput(attrs={'placeholder': 'sara.email@email.com'}))
     username = forms.CharField(label="Username", max_length=15, widget=forms.TextInput(attrs={'placeholder': 'sarakhan'}))
     password = forms.CharField(label="Password", max_length=8, widget=forms.PasswordInput(attrs={'placeholder': '********'}))
