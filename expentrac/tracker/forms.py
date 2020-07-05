d0c4bd4258d54039270977f497d302b5915f64b4
Sania <sania.d@myplanet.com>
2020-07-05 13:05:26 -0400

    Removed the help text from the login form and applied to the signup form

--- expentrac/tracker/forms.py
+++ expentrac/tracker/forms.py
@@ -2,17 +2,17 @@ from django import forms
 
 
 class LoginForm(forms.Form):
-    username = forms.CharField(label="Username", max_length=15, help_text='15 characters max.', widget=forms.TextInput(attrs={'placeholder': 'sarakhan'}))
-    password = forms.CharField(label="Password", max_length=12, help_text='8 characters min, 12 characters max. There must be two numbers and two special characters each.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
+    username = forms.CharField(label="Username", max_length=15, widget=forms.TextInput(attrs={'placeholder': 'sarakhan'}))
+    password = forms.CharField(label="Password", max_length=12, widget=forms.PasswordInput(attrs={'placeholder': '********'}))
 
 
 class SignUpForm(forms.Form):
-    first_name = forms.CharField(label="First name", max_length=20, widget=forms.TextInput(attrs={'placeholder': 'sara'}))
-    last_name = forms.CharField(label="Last name", max_length=30, required=False, widget=forms.TextInput(attrs={'placeholder': 'khan'}))
+    first_name = forms.CharField(label="First name", max_length=20, help_text='20 characters max.', widget=forms.TextInput(attrs={'placeholder': 'sara'}))
+    last_name = forms.CharField(label="Last name", max_length=30, help_text='30 characters max.', required=False, widget=forms.TextInput(attrs={'placeholder': 'khan'}))
     email = forms.EmailField(label="Email", max_length=60, widget=forms.EmailInput(attrs={'placeholder': 'sara.email@email.com'}))
-    username = forms.CharField(label="Username", max_length=15, widget=forms.TextInput(attrs={'placeholder': 'sarakhan'}))
-    password = forms.CharField(label="Password", max_length=8, widget=forms.PasswordInput(attrs={'placeholder': '********'}))
-    cPassword = forms.CharField(label="Confirm Password", max_length=8, widget=forms.PasswordInput(attrs={'placeholder': '********'}))
+    username = forms.CharField(label="Username", max_length=15, help_text='15 characters max.', widget=forms.TextInput(attrs={'placeholder': 'sarakhan'}))
+    password = forms.CharField(label="Password", max_length=8, help_text='8 characters min, 12 characters max. There must be two numbers and two special characters each.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
+    cPassword = forms.CharField(label="Confirm Password", help_text='This must match the previous password field.', max_length=8, widget=forms.PasswordInput(attrs={'placeholder': '********'}))
 
 
 
