510cbc745fe48ff12a91f006d584c3e2097e92af
Sania <sania.d@myplanet.com>
2020-07-12 16:56:58 -0400

    Apply password validators initially

--- expentrac/tracker/forms.py
+++ expentrac/tracker/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth.password_validation import validate_password
 from django.forms import DateInput
 
 from .models import Expense
@@ -15,13 +16,16 @@ class SignUpForm(forms.Form):
     last_name = forms.CharField(label="Last name", max_length=30, help_text='30 characters max.', required=False, widget=forms.TextInput(attrs={'placeholder': 'khan'}))
     email = forms.EmailField(label="Email", max_length=60, widget=forms.EmailInput(attrs={'placeholder': 'sara.email@email.com'}))
     username = forms.CharField(label="Username", max_length=15, help_text='15 characters max.', widget=forms.TextInput(attrs={'placeholder': 'sarakhan'}))
-    password = forms.CharField(label="Password", min_length=8, max_length=12, help_text='8 characters min, 12 characters max. There must be two numbers and two special characters each.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
-    cPassword = forms.CharField(label="Confirm Password", min_length=8, max_length=12, help_text='This must match the previous password field.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
+    password = forms.CharField(label="Password", min_length=9, max_length=12, help_text='9 characters min, 12 characters max. There must be two numbers and two special characters each.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
+    cPassword = forms.CharField(label="Confirm Password", min_length=9, max_length=12, help_text='This must match the previous password field.', widget=forms.PasswordInput(attrs={'placeholder': '********'}))
+
 
     def clean(self):
         cleaned_data = super(SignUpForm, self).clean()
         password = self.cleaned_data.get('password')
         cpassword = self.cleaned_data.get('cPassword')
+
+        # validate_password(password)    # Django's password validation
 
         if password and cpassword and password != cpassword:
             raise forms.ValidationError("Passwords do not match")
